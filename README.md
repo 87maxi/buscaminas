b9ddde9d97c0001cd0522f9138e6d7ab41f5a232
maxi <maxiparedes87@gmail.com>
2017-09-09 11:44:53 -0300

    firts commit api

---
+++ README.md
@@ -0,0 +1,11 @@
+#buscaminas test
+
+
+
+####load api
+
+pip install -r requirements.txt
+
+cd api/
+FLASK_APP=buscaminas.py flask run
+	
---
+++ api/buscaminas.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+import random 
+
+app = Flask(__name__)
+#Todo create virtuialhost
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/panel")
+def panel():
+    panel = []
+    mine_limit=9
+    panel_n = 10
+
+    idx=0
+    for y in range(panel_n):
+        y += 1
+        for x in range(panel_n):
+            x +=1
+            mine = False
+            if len([p["mine"] for p in panel if p['mine'] == True]) < mine_limit:
+                mine = random.choice([True, False])
+            idx+=1
+            panel.append(dict(x=x, y=y, mine=mine, idx=idx))
+
+    return jsonify(panel)
