90d1fc54d026b4c6ca805434b77e92a10874bc0f
maxi <maxiparedes87@gmail.com>
2017-09-12 19:15:58 -0300

    more

--- client/assets/js/panel.js
+++ client/assets/js/panel.js
@@ -1,18 +1,4 @@
 
-/*
-* Todo Obj  Loader init endpoints 
-*/
-
-$.getJSON("http://localhost:5000/panel").done(function(data){
-    draft_panel(data);
-    user_events(data);
-       
-    //timer
-    start=new Date();
-    setInterval(draft_timer,1000);
-    console.log('success')
-})
-
 
 /*
 *ToDO Obj draft 
@@ -37,12 +23,12 @@ function draft_panel (panel){
     panel.forEach(
       function(e){
         mine = e.mine ? "b" : "n"  
-      $("#row_"+e.y).append('<td  class="cell col_'+e.x+'" data-idx="'+e.idx+'" >'+mine+'</td>')
+      $("#row_"+e.y).append('<td  class="cell covered col_'+e.x+'" data-idx="'+e.idx+'" >'+mine+'</td>')
     })
 }
 
 function draft_timer(){
-  $("#timer").html(timer())
+  $("#timer").html(Utils.timer())
 }
 
 
@@ -50,23 +36,34 @@ function draft_timer(){
 ToDo Obj events
 */
 
-function user_events(panel){
-  $(".cell").click(function(){  
-      panel.forEach(
-           function(e){
-            if(e.idx == this.dataset.idx ){
-              action = e.mine ? "game_over()" : "cell_discover()"
-              alert(action)
-            } 
-      }, this)
-  })
+var EventUser = {};
+		 
+EventUser.click_panel = function (panel){
+
+					$(".cell").click(function(){  
+								Utils.panel.forEach(
+									 function(e){
+										if(e.idx == this.dataset.idx ){
+
+											//if (e.mine){
+														Utils.filter_cell(e, Utils.discover_cell)
+										 //}
+
+							//action = e.mine ? "game_over()" : cell_discover(panel,  e )
+							} 
+				}, this)
+
+		})
 }
+
+
+
+
 
 
 /*
 function game_over(){} *5
 
-function cell_discover(){} *3
 
 function set_flg(){} *4
 
@@ -76,14 +73,58 @@ function set_ask(){} *4
 /*
 *Todo obj Utils
 */
-   
-function timer() { 
+
+var Utils = {};
+
+Utils.timer = function () { 
          var time=new Date(); 
          var cro=time-start; 
          var cr=new Date(); 
          cr.setTime(cro); 
          var sg=cr.getSeconds(); 
          var mn=cr.getMinutes(); 
-        return mn+" : "+sg; 
+         return mn+" : "+sg; 
+};
+
+
+Utils.filter_cell = function (e, callback){
+    var filter = [];
+		this.panel.filter(
+						function(obj){
+								var  X = [e.x+1, e.x, e.x-1]; 
+								var	 Y = [e.y+1, e.y, e.y-1];
+								if (X.indexOf(obj.x) != -1 && Y.indexOf(obj.y) != -1){
+									filter.push(obj)
+								}
+						}, e);
+	
+	    return callback ? callback(filter) : filter;
+};
+
+Utils.discover_cell = function(e){
+		e.forEach(function(obj){
+			elem = document.querySelector("[data-idx~='"+obj.idx+"']")	
+			elem.classList.remove("covered")
+	})
 }
+
+
+
+
+/*
+* Todo Obj  Loader init endpoints 
+*/
+
+$.getJSON("http://localhost:5000/panel").done(function(data){
+    
+	  Utils.panel = data;
+		draft_panel(data);
+    //user_events(data);
+	  EventUser.click_panel();
+       
+    //timer
+    start=new Date();
+    setInterval(draft_timer,1000);
+    console.log('success')
+})
 
